606f405776c9e06392cd78ecddd0e4cd79d289b8
Ashutosh Choudhary <ashutoshchoudhary.job@gmail.com>
2024-10-13 22:15:19 +0530

    fixed bug where city could not be found

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-    $city="patna";
+    $weather = "";
+    $error = "";
+    if(array_key_exists('city', $_GET)) {
+        $city = str_replace(' ', '', $_GET['city']);
     $url = "https://www.timeanddate.com/weather/?query=" . $city;
 
     // Get headers for the specified URL
@@ -20,19 +23,23 @@
     libxml_clear_errors();
 
     $temperatureElements = $dom->getElementsByTagName('td');
-
-    $xpath = new DOMXPath($dom);
-
-    foreach ($temperatureElements as $element) {
-        // Check if the class name contains 'temperature'
-        if (strpos($element->getAttribute('class'), 'rbi') !== false) {
-            // Output the temperature text
-            $temperature = $element->nodeValue;
-            echo "Temperature: " . trim($element->nodeValue) . "\n";
-            $weather=$temperature;
-            break;
+    if ($temperatureElements->length > 0) {
+        $error = "No City found";
+    }else{
+        foreach ($temperatureElements as $element) {
+            // Check if the class name contains 'temperature'
+            if (strpos($element->getAttribute('class'), 'rbi') !== false) {
+                // Output the temperature text
+                $temperature = $element->nodeValue;
+                $weather=$temperature;
+                break;
+            }
         }
+
     }
+
+    
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -42,19 +49,32 @@
     <title>Weather Scrapper</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <body>
+
     <div class="container">
-            <form method="GET" action="">
+            <form method="get" action="">
                 <div class="mb-3 form-group">
                   <label for="city" class="form-label">Enter a City</label>
-                  <input type="text" class="form-control" id="city" placeholder="Eg. London, Tokyo" aria-describedby="emailHelp">
+                  <input type="text" class="form-control" name='city' id="city" placeholder="Eg. London, Tokyo" aria-describedby="emailHelp" value="<?php
+                if(array_key_exists('city', $_GET)) {
+                    echo $_GET['city'];
+                }
+                ?>">
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>        
-        
+        <div id="weather">
+            <?php
+                if($weather) {
+                    echo '<div class="alert alert-success" role="alert">'.$weather.'</div>';
+                } else  if($error) {
+                    echo '<div class="alert alert-danger" role="alert">'.$error.'</div>';
+                }
+            ?>
+        </div>
     </div>
 </head>
-<body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
